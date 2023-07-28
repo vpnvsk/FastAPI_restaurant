c5a417ebc9b1e75489c0035eb36cfd9e956cb946
vpnvsk <vlad.pnevsk@gmail.com>
2023-07-28 08:35:46 -0400

    change items quantity feature

--- app/cart/router.py
+++ app/cart/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select, insert, join, func
+from sqlalchemy import select, update, join, delete
+from sqlalchemy.future import select as future_select
+from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
@@ -7,6 +9,7 @@ from auth.models import User
 from auth.base_config import current_user
 from items.models import item
 from cart.models import cart, cart_item
+from cart.schemas import ChangeQuantity
 
 
 router = APIRouter(
@@ -36,3 +39,40 @@ async def get_cart_items(session: AsyncSession = Depends(get_async_session), use
     }
 
     return response
+
+@router.post('/')
+async def change_quantity_of_product(option:ChangeQuantity,
+                                  session: AsyncSession = Depends(get_async_session),
+                                     user: User=Depends(current_user)):
+        
+
+    query = select(cart_item.c.quantity, cart_item.c.id).select_from(
+        join(item, cart_item, item.c.id == cart_item.c.item_id)
+        .join(cart, cart_item.c.cart_id == cart.c.id)).where(cart.c.user_id == user.id,
+                                                            cart.c.is_ordered == False, item.c.name == option.name)
+    res = await session.execute(query)
+
+    cart_item_row = res.fetchone()
+
+
+    if cart_item_row:
+        quantity_value, cart_item_id = cart_item_row
+
+        match option.status:
+            case option.status.incremente:
+                quantity_value = quantity_value + 1
+            case option.status.decremente:
+                quantity_value = quantity_value - 1
+            case option.status.delete:
+                stmt = delete(cart_item).where(cart_item.c.id == cart_item_id)
+                res = await session.execute(stmt)
+                await session.commit()
+                return {"mesage":"item has been deleted"}
+
+
+        stmt = update(cart_item).where(cart_item.c.id == cart_item_id).values(quantity=quantity_value)
+        await session.execute(stmt)
+        await session.commit()
+        return {"message": "Quantity updated successfully"}
+
+    raise HTTPException(status_code=404, detail="Item not found in the cart")
